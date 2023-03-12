407ae36b6d8110ada0d7824a7c10cfad435226a2
Morgana <morganabagatini@gmail.com>
2023-03-11 22:58:40 -0300

    Code refactoring

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,9 +1,140 @@
-describe("Gilded Rose", function() {
+describe("Gilded Rose", function () {
+	describe("when updating Sulfuras item", () => {
+		it("should not descrease in quality", function () {
+			const items = [new Item("Sulfuras, Hand of Ragnaros", 30, 80)];
+			const inventory = new Inventory(items)
+			inventory.update_quality();
 
-  it("should foo", function() {
-    items = [ new Item("foo", 0, 0) ];
-    update_quality();
-    expect(items[0].name).toEqual("fixme");
-  });
+			expect(items[0].quality).toEqual(80);
+		})
+
+		it("should not descrease in sell_in", function () {
+			const items = [new Item("Sulfuras, Hand of Ragnaros", 30, 80)];
+			const inventory = new Inventory(items)
+			inventory.update_quality();
+
+			expect(items[0].sell_in).toEqual(30);
+		})
+	})
+
+	describe("when updating Aged Brie item", () => {
+		it("should increase in quality by 1", function () {
+			const items = [new Item("Aged Brie", 10, 10)];
+			const inventory = new Inventory(items)
+			inventory.update_quality();
+
+			expect(items[0].quality).toEqual(11);
+		})
+
+		it("should descrease in sell_in by 1", function () {
+			const items = [new Item("Aged Brie", 10, 10)];
+			const inventory = new Inventory(items)
+			inventory.update_quality();
+
+			expect(items[0].sell_in).toEqual(9);
+		})
+
+		it("quality should not be greater than 50", function () {
+			const items = [new Item("Aged Brie", 10, 50)];
+			const inventory = new Inventory(items)
+			inventory.update_quality();
+
+			expect(items[0].quality).toEqual(50);
+		})
+	})
+
+	describe("when updating Backstage passes item", () => {
+		it("should increase in quality by 1", function () {
+			const items = [new Item("Backstage passes to a TAFKAL80ETC concert", 15, 10)];
+			const inventory = new Inventory(items)
+			inventory.update_quality();
+
+			expect(items[0].quality).toEqual(11);
+		})
+
+		it("should increase in quality by 2", function () {
+			const items = [new Item("Backstage passes to a TAFKAL80ETC concert", 8, 10)];
+			const inventory = new Inventory(items)
+			inventory.update_quality();
+
+			expect(items[0].quality).toEqual(12);
+		})
+
+		it("should increase in quality by 3", function () {
+			const items = [new Item("Backstage passes to a TAFKAL80ETC concert", 5, 10)];
+			const inventory = new Inventory(items)
+			inventory.update_quality();
+
+			expect(items[0].quality).toEqual(13);
+		})
+
+		it("the quality should be 0", function () {
+			const items = [new Item("Backstage passes to a TAFKAL80ETC concert", -1, 10)];
+			const inventory = new Inventory(items)
+			inventory.update_quality();
+
+			expect(items[0].quality).toEqual(0);
+		})
+
+		it("should decrease in sell_in by 1", function () {
+			const items = [new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10)];
+			const inventory = new Inventory(items)
+			inventory.update_quality();
+
+			expect(items[0].sell_in).toEqual(-1);
+		})
+
+		it("quality should not be greater than 50", function () {
+			const items = [new Item("Backstage passes to a TAFKAL80ETC concert", 10, 50)];
+			const inventory = new Inventory(items)
+			inventory.update_quality();
+
+			expect(items[0].quality).toEqual(50);
+		})
+	})
+
+	describe("when updating Conjured item", () => {
+		it("should decrease in sell_in by 1", function () {
+			const items = [new Item("Conjured Mana Cake", 0, 10)];
+			const inventory = new Inventory(items)
+			inventory.update_quality();
+
+			expect(items[0].sell_in).toEqual(-1);
+		})
+
+		it("should decrease in quality by 2", function () {
+			const items = [new Item("Conjured Mana Cake", 10, 10)];
+			const inventory = new Inventory(items)
+			inventory.update_quality();
+
+			expect(items[0].quality).toEqual(8);
+		})
+	})
+
+	describe("when updating standard item", () => {
+		it("should decrease in sell_in by 1", function () {
+			const items = [new Item("Ordinary food", 10, 10)];
+			const inventory = new Inventory(items)
+			inventory.update_quality();
+
+			expect(items[0].sell_in).toEqual(9);
+		})
+
+		it("should decrease in quality by 1", function () {
+			const items = [new Item("Ordinary food", 10, 10)];
+			const inventory = new Inventory(items)
+			inventory.update_quality();
+
+			expect(items[0].quality).toEqual(9);
+		})
+
+		it("should decrease in quality by 2", function () {
+			const items = [new Item("Ordinary food", -2, 10)];
+			const inventory = new Inventory(items)
+			inventory.update_quality();
+
+			expect(items[0].quality).toEqual(8);
+		})
+	})
 
 });
--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -1,55 +1,149 @@
 function Item(name, sell_in, quality) {
-  this.name = name;
-  this.sell_in = sell_in;
-  this.quality = quality;
+	this.name = name;
+	this.sell_in = sell_in;
+	this.quality = quality;
 }
 
-var items = []
+class Inventory {
+	#items = []
 
-function update_quality() {
-  for (var i = 0; i < items.length; i++) {
-    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-      if (items[i].quality > 0) {
-        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          items[i].quality = items[i].quality - 1
-        }
-      }
-    } else {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1
-        if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].sell_in < 11) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-          if (items[i].sell_in < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-        }
-      }
-    }
-    if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-      items[i].sell_in = items[i].sell_in - 1;
-    }
-    if (items[i].sell_in < 0) {
-      if (items[i].name != 'Aged Brie') {
-        if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].quality > 0) {
-            if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              items[i].quality = items[i].quality - 1
-            }
-          }
-        } else {
-          items[i].quality = items[i].quality - items[i].quality
-        }
-      } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
-        }
-      }
-    }
-  }
+	constructor(items) {
+		this.#items = items
+	}
+
+	#update_quality_by_category(item) {
+		if (item.name == 'Sulfuras, Hand of Ragnaros') {
+			return this.#update_quality_legendary(item)
+		}
+		if (item.name == 'Aged Brie') {
+			return this.#update_quality_aged_brie(item)
+		}
+		if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+			return this.#update_quality_backstage(item)
+		}
+		if (item.name == 'Conjured Mana Cake') {
+			return this.#update_quality_conjured(item)
+		}
+
+		return this.#update_quality_standard(item)
+	}
+
+	update_quality() {
+		for (var i = 0; i < this.#items.length; i++) {
+			this.#items[i] = this.#update_quality_by_category(this.#items[i])
+
+			// old code 
+			/*
+			if (this.#items[i].name != 'Aged Brie' && this.#items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+				if (this.#items[i].quality > 0) {
+					if (this.#items[i].name != 'Sulfuras, Hand of Ragnaros') {
+						this.#items[i].quality = this.#items[i].quality - 1
+					}
+				}
+			} else {
+				if (this.#items[i].quality < 50) {
+					this.#items[i].quality = this.#items[i].quality + 1
+					if (this.#items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+						if (this.#items[i].sell_in < 11) {
+							if (this.#items[i].quality < 50) {
+								this.#items[i].quality = this.#items[i].quality + 1
+							}
+						}
+						if (this.#items[i].sell_in < 6) {
+							if (this.#items[i].quality < 50) {
+								this.#items[i].quality = this.#items[i].quality + 1
+							}
+						}
+					}
+				}
+			}
+			if (this.#items[i].name != 'Sulfuras, Hand of Ragnaros') {
+				this.#items[i].sell_in = this.#items[i].sell_in - 1;
+			}
+			if (this.#items[i].sell_in < 0) {
+				if (this.#items[i].name != 'Aged Brie') {
+					if (this.#items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+						if (this.#items[i].quality > 0) {
+							if (this.#items[i].name != 'Sulfuras, Hand of Ragnaros') {
+								this.#items[i].quality = this.#items[i].quality - 1
+							}
+						}
+					} else {
+						this.#items[i].quality = this.#items[i].quality - this.#items[i].quality
+					}
+				} else {
+					if (this.#items[i].quality < 50) {
+						this.#items[i].quality = this.#items[i].quality + 1
+					}
+				}
+			}
+			*/
+		}
+	}
+
+	#decrease_sell_in(item) {
+		return item.sell_in - 1
+	}
+
+	#decrease_quality(item, number = 2) {
+		if (item.quality - number < 0) {
+			return 0
+		}
+
+		return item.quality - number
+	}
+
+	#inscrease_quality(item, number = 1) {
+		if (item.quality + number > 50) {
+			return 50
+		}
+
+		return item.quality + number
+	}
+
+	#update_quality_legendary(item) {
+		return item
+	}
+
+	#update_quality_aged_brie(item) {
+		item.sell_in = this.#decrease_sell_in(item)
+		item.quality = this.#inscrease_quality(item)
+
+		return item
+	}
+
+	#update_quality_backstage(item) {
+		item.sell_in = this.#decrease_sell_in(item)
+
+		if (item.sell_in < 0) {
+			item.quality = 0
+		} else if (item.sell_in > 5 && item.sell_in <= 10 ) {
+			item.quality = this.#inscrease_quality(item, 2)
+		} else if (item.sell_in >= 0 && item.sell_in <= 5 ) {
+			item.quality = this.#inscrease_quality(item, 3)
+		} else {
+			item.quality = this.#inscrease_quality(item, 1)
+		}
+
+		return item
+	}
+
+	#update_quality_conjured(item) {
+		item.sell_in = this.#decrease_sell_in(item)
+		item.quality = this.#decrease_quality(item, 2)
+
+		return item
+	}
+
+	#update_quality_standard(item) {
+		item.sell_in = this.#decrease_sell_in(item)
+
+		if (item.sell_in < 0) {
+			item.quality = this.#decrease_quality(item, 2)
+		} else {
+			item.quality = this.#decrease_quality(item, 1)
+		}
+
+		return item
+	}
 }
